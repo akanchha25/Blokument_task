ab202477ec307a1699ab054ed0380b4b2bb13cc3
akanchha25 <75972629+akanchha25@users.noreply.github.com>
2022-12-23 11:18:24 +0530

    Update user.js

--- login task/controllers/user.js
+++ login task/controllers/user.js
@@ -110,7 +110,9 @@ exports.varifyToken= async (req,res,next) =>{
 }
 const mydetail = async (req, res) => {
     const fetch = req.user_details;
-    pool.queries(queries.mydetail,fetch.ID,(error,results) =>{
+    pool.queries(queries.mydetail,fetch.ID,(error,results) =>{    //mydetail is query from queries.js file (const mydetail = "SELECT address_details.ID, address, city, state, pincode FROM user_details Right JOIN address_details ON address_details.ID = user_details.ID WHERE user_details.ID = '${user_details.ID}'";
+
+
         const Info = results.rows.length;
 
         if(!Info){
