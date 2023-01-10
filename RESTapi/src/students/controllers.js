f2028d74e5eae079030fc7c11662e47d478760e8
akanchha25 <akanchhakumari11@gmail.com>
2023-01-10 19:28:12 +0530

    task done

--- RESTapi/src/students/controllers.js
+++ RESTapi/src/students/controllers.js
@@ -4,8 +4,12 @@ const queries = require('./queries');
 
 const getStudents = (req, res) =>{
     pool.query(queries.getStudents, (error,results) =>{
+
+        
         if (error) throw error;
         res.status(200).json(results.rows);
+
+
     });
 };
 
